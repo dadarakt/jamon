3b6154cc7d9ea3f0836afe900a3d5c5ca8637697
Simon Danisch <sdanisch@gmail.com>
2014-04-29 17:15:07 +0200

    text eding works (maybe without bugs)

--- src/main/julia/editor/editor_new.jl
+++ src/main/julia/editor/editor_new.jl
@@ -3,58 +3,57 @@ import GLUtil.render
 
 scroll(event, t) = t.y += event.key == 4 ? -40 : 40
 
+function delete(s::String, r::Range{Int})
+	i = chr2ind(s, first(r))
+	j = chr2ind(s, last(r))
+	s[1:prevind(s, i)] * s[nextind(s, j):end]
+end
 
-function deleteChar(event::KeyDown, t::TextField)
-	i = first(t.selection)
+function delete(s::String, i::Int)
+	if i == 0
+		return s
+	elseif i == length(s)
+		return s[1:end-1]
+	end
+	I = chr2ind(s, i)
+	return s[1:prevind(s, I)] * s[nextind(s, I):end]
+end
+
+function delete(event::KeyDown, t::TextField)
 	if length(t.selection) > 0
-		t.text = t.text[chr2ind(t.text, i):chr2ind(t.text, last(t.selection))]
+		t.text = delete(t.text, t.selection)
+	else
+		t.text = delete(t.text, first(t.selection))
 	end
-	Imin1 = chr2ind(t.text, i - 1)
-	I = Imin1 + (isascii(t.text[Imin1]) ? 1 : 2)
-
-	if i > 1 && i < length(t.text)
-		IPlus1 	= I + (isascii(t.text[I]) ? 1 : 2)
-		a = t.text[1:Imin1]
-		b = t.text[IPlus1:end]
-	elseif i == 1 && i < length(t.text)
-		IPlus1 	= I + (isascii(t.text[I]) ? 1 : 2)
-		a = ""
-		b = t.text[IPlus1:end]
-	elseif i >= 1 && i == length(t.text)
-		a = t.text[1:Imin1]
-		b = ""
-	elseif i == 1 && length(t.text) == 1
-		a = ""
-		b = ""
-	end
-	t.selection = i-1:1-2
-	t.text = a * b
+	t.selection = first(t.selection)-1:first(t.selection)-2
 	update(t)
 end
-function newLine(event::KeyDown, t::TextField)
-	addChar(event::KeyDown, t::TextField)
+
+function newline(event::KeyDown, t::TextField)
+	addchar(event, t)
 end
-function addChar(event::KeyDown, t::TextField)
-	a = ""
-	b = ""
+
+addchar(s::String, char::Char, i::Int) = addchar(s, string(char), i)
+
+function addchar(s::String, char::String, i::Int)
+	if i == 0
+		return char * s
+	elseif i == length(s)
+		return s * char
+	end
+	I 		= chr2ind(s, i)
+	startI 	= nextind(s, I)
+	return s[1:I] *char* s[startI:end]
+end
+
+function addchar(event::KeyDown, t::TextField)
+	char 
 	i = first(t.selection)
 	if length(t.selection) > 0
-		t.text = t.text[chr2ind(t.text, i):chr2ind(t.text, last(t.selection))]
+		t.text = delete(t.text, t.selection)
 	end
-	if i >= 1 && i < length(t.text)
-		lastI = chr2ind(t.text, i)
-		startI = lastI + (isascii(t.text[lastI]) ? 1 : 2)
-		a = t.text[1:lastI]
-		b = t.text[startI:end]
-	elseif i >= 1 && i == length(t.text)
-		a = t.text
-	elseif i == 0
-		b = t.text
-	end
-
-	t.text = a * string(event.key) * b
+	t.text = addchar(t.text, event.key, i)
 	t.selection = i+1:i
-
 	update(t)
 end
 
@@ -149,18 +148,15 @@ function select(direction::ASCIIString, t::TextField, f::FontProperties)
 end
 
 
-
-
-
 createWindow()
 
 font 	= getFont()
 text 	= (join([i for i=0:9],"")*"\n")^10
 t 		= TextField(text, 10, 500)
 
-registerEventAction(EventAction{KeyDown{0}}(x -> !x.special && x.key == '\b', (), deleteChar, (t,)))
-registerEventAction(EventAction{KeyDown{0}}(x -> !x.special && isprint(x.key), (), addChar, (t,)))
-registerEventAction(EventAction{KeyDown{0}}(x -> x.special && x.key == '\n' || x.key == '\r', (), newLine, (t,)))
+registerEventAction(EventAction{KeyDown{0}}(x -> !x.special && x.key == '\b', (), delete, (t,)))
+registerEventAction(EventAction{KeyDown{0}}(x -> !x.special && isprint(x.key), (), addchar, (t,)))
+registerEventAction(EventAction{KeyDown{0}}(x -> x.special && x.key == '\n' || x.key == '\r', (), newline, (t,)))
 
 registerEventAction(EventAction{MouseClicked{0}}(x -> x.key == 0 && x.status == 0, (), select, (t, font.properties)))
 registerEventAction(EventAction{KeyUp{0}}(
